7922629ca763e4f13a0102e0d2a7e877b0eb6526
freta <itsmefruta@gmail.com>
2022-08-12 12:28:49 +0300

    Display the result in the site itself

--- script.js
+++ script.js
@@ -4,11 +4,9 @@ let computerScore= 0;
 const rock= document.querySelector('#rock');
 const paper = document.querySelector('#paper');
 const scissors= document.querySelector('#scissors');
-const buttons = document.querySelectorAll('button');
-const container = document.getElementById('result');
-
-
-
+const rescont = document.querySelector('#resultcon');
+const div1 = document.createElement('div');
+const div2 = document.createElement('div');
 
 
 
@@ -37,77 +35,68 @@ function playRound(playerSelection, computerSelection){
          else if (playerSelection === 'rock' && computerSelection === 'paper'){
             computerScore++;
             return 'Computer Win, paper beats rock'; 
-            
        }
     // player rock, computer scissors
         else if(playerSelection === 'rock' && computerSelection === 'scissors' ){
             playerScore++;
             return 'Player win!, Rock beats Scissors';
-            
         }
     // player scissors, computer rock
         else if ( playerSelection === 'scissors' && computerSelection === 'rock' ){
             computerScore++;
              return  'Computer Win, rock beats scissors';
-          
           }
     // player scissors, computer paper
         else if ( playerSelection === 'scissors' && computerSelection === 'paper'){
             playerScore++;
             return 'Player Win, scissors beats paper';
-            
         }
     // player paper, computer scissors
     else if (playerSelection === 'paper' && computerSelection === 'scissors'){
            computerScore++;
            return 'Computer Win, scissors beats paper';
-           
          }
     // player paper, computer rock
          else if (playerSelection === 'paper' && computerSelection === 'rock'){
             playerScore++;
             return 'Player win, paper beats rock';
-                
              }
      }
 
 let game = () => {
     playerScore =0 , computerScore =0;
-         // for (let i= 0 ; i < 5; i++){
-            // selecting all the three buttons
-            
-            [rock,paper,scissors].forEach((button) => {
+    //div1.parentNode.removeChild(div1);
+     //div2.parentNode.removeChild(div2);
+    // rescont.removeChild(div1);
+    // rescont.removeChild(div2);
+                 [rock,paper,scissors].forEach((button) => {
                 button.addEventListener('click', (e) => {
-                    containDisapper(container, button.id); 
-                    
+                 containDisapper(container, button.id); 
+                  
                 });
             });
-            // console.log(playerScore,computerScore);
-            //  console.log(scores());// displays the result after the five round
             
 }
 
 function containDisapper(container, sel){
-    
+  
     const playerSelection = sel;
     const computerSelection = computerPlay();
     const result= playRound(playerSelection, computerSelection);
     console.log(playerSelection,computerSelection);
-    console.log( `Player selected ${playerSelection}, Computer selected ${computerSelection}`);
+    console.log( `Player selected ${playerSelection}, Computer selected ${computerSelection} `);
     console.log(result);
-    const div1 = document.createElement('div');
-    const selection = document.createTextNode(`Player selected ${playerSelection}, Computer selected ${computerSelection}`);
+    const selection = document.createTextNode(`Player selected ${playerSelection}, Computer selected ${computerSelection} `);
     const score = document.createTextNode(result);
-   
+    rescont.appendChild(div1);
+    rescont.appendChild(div2);
+    div1.appendChild(selection);
+    div2.appendChild(score);
+  
     
-    container.appendChild(div1);
-    const div2 = document.createElement('div1');
-   // div1.appendChild(div2);
-    div1.appendChild(score);
-   div2.appendChild(selection);
-   //div1.remove();
-   
-}
+    
+    
+   }
 
     function scores(){
         if(playerScore > computerScore){
